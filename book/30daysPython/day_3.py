d998e6adf313de668fa0b33d8ae1038d74ce1b89
CHpass <orellana.c.p.a.91@gmail.com>
2024-11-28 06:03:30 -0400

    Dia 3 python en 30 días, avanzado hasta el ejercicio 5

---
+++ book/30daysPython/day_3.py
@@ -0,0 +1,14 @@
+age = 33
+height = 1.85
+compx = (2 * 3j)
+
+base = input('Enter the base of the triangle: ')
+height = input('Enter the hight of the triangle: ')
+area_triangle = 0.5 * float(base) * float(height)
+print(f'The area of the triangle is: {area_triangle}')
+
+side_a = float(input('Enter side a of the triangle: '))
+side_b = float(input('Enter side b of the triangle: '))
+side_c = float(input('Enter side c of the triangle: '))
+perimeter_triangle = side_a + side_b + side_c
+print(f'The perimeter of the triangle is: {perimeter_triangle}')
